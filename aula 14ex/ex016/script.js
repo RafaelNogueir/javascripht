33dad6cf520bfe45fbe2b3b9e95f3d475dafff36
RafaelNogueir <rafael-oliveira05@live.com>
2022-06-19 07:47:36 -0300

    aula 14

---
+++ aula 14ex/ex016/script.js
@@ -0,0 +1,33 @@
+function contar (){
+    var inicio = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert(`[ERRO] Faltam dados!`)
+        res.innerHTML = `Impossivel Contar!!`
+    }else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if(p <= 0){
+            alert('Passo Invalido! Considerando PASSO 1')
+            passo = 1
+        }
+        if(i < f){
+            //Contagem Crescente
+            for(var c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+            //Contagem Regressiva
+        }else{
+            for (var c = i; c>=f; c-=p ){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }res.innerHTML += `\u{1F3C1}`
+        }
+        
+    }
+}
